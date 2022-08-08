76189e646002c2dd51d47f1b1111797188678863
Karoline Nascimento França <karolinenascimento777@gmail.com>
2022-08-08 19:11:47 -0300

    Iniciando projeto

---
+++ SeguraeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeguraeApp
+//
+//  Created by Karoline Do Nascimento on 08/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
