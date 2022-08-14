30e20cedeba8c8fff2f3aa7bdf28794e88056946
Vinicius Bin <binvini@gmail.com>
2022-08-14 13:57:31 -0300

    Telas criacao de conta

---
+++ SeguraeApp/LoginCreateAccount/CreateAccountViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CreateAccountViewController.swift
+//  SeguraeApp
+//
+//  Created by Vinicius on 14/08/22.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+    @IBOutlet weak var mainLoginImageView: UIImageView!
+    @IBOutlet weak var createAccountButton: UIButton!
+    @IBOutlet weak var doLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SeguraeApp/LoginCreateAccount/PasswordViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PasswordViewController.swift
+//  SeguraeApp
+//
+//  Created by Vinicius on 14/08/22.
+//
+
+import UIKit
+
+class PasswordViewController: UIViewController {
+    @IBOutlet weak var createAccountImageView: UIImageView!
+    @IBOutlet weak var createAccountPasswordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
