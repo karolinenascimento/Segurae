20cc4dc0de0cc5a849e4860f2fd12bd3b6ce76c9
Vinicius Bin <binvini@gmail.com>
2022-08-16 19:04:52 -0300

    mais configuracoes criacao de contas e login

--- SeguraeApp/LoginCreateAccount/CreateAccountViewController.swift
+++ SeguraeApp/LoginCreateAccount/CreateAccountViewController.swift
@@ -14,19 +14,15 @@ class CreateAccountViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureUIcomponents()
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureUIcomponents() {
+        mainLoginImageView.layer.borderWidth = 2.0
+        mainLoginImageView.layer.cornerRadius = 10
+        mainLoginImageView.layer.borderColor = UIColor.green.cgColor
     }
-    */
 
 }
